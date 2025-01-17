403f34824f88f4d1c5087a094edf3d423695d45b
kanga-prog <kangabricekouakou@gmail.com>
2025-01-17 20:07:22 +0000

    1-calculation.py

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    # Affichage des résultats avec formatage
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
