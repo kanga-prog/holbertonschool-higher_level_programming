87391757cea27a537d21380a87eeb60d185181f8
kanga-prog <kangabricekouakou@gmail.com>
2025-01-17 15:37:17 +0000

    12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 101):
-        if i % 3 == 0 :
-           print("fizz",end=" ")
-        elif i % 5 == 0 :
-           print("buzz",end=" ")
-        elif i % 3 == 0 & i % 5 == 0:
-           print("fizzbuzz", end=" ")
+    for i in range(1, 101):  # Boucle de 1 à 100
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
         else:
-           print(i, end="")
+            print(i, end=" ")
+            
