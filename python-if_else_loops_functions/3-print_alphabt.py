1de0abca1bc47066b932dd4617619a64aea27417
kanga-prog <9952@holbertonstudents.com>
2025-01-17 01:09:42 +0100

    3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-for i in range(97, 123):
-    if i != 101 and i != 113:
-        print("{}".format(chr(i), end=''))
-
+for alphabet in range(97, 123):
+    if alphabet != 113 and alphabet != 101:
+        print("{}".format(chr(alphabet)), end="")
