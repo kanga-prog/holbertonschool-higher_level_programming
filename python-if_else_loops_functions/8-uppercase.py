b4532815018aac630201cbc4d5e99e736baec44e
kanga-prog <9952@holbertonstudents.com>
2025-01-17 13:55:58 +0100

    8-uppercase.py

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:  # Si c'est une minuscule
+            char = chr(ord(char) - 32)  # Convertir en majuscule
+        print("{}".format(char), end="")  # Afficher le caractère (converti ou non)
+    print()  # Ajouter un saut de ligne à la fin
+
