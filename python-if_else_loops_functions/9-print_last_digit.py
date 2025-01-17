e0863daf35f2437e5b969c43229b767a66a61735
kanga-prog <kangabricekouakou@gmail.com>
2025-01-17 14:45:42 +0000

    9-print_last_digit.py

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,10 +2,7 @@
 
 def print_last_digit(number):
     if number < 0:
-        number = -number
-        last_digit = number % 10
-        print(last_digit, end="")
-        return last_digit
-
-
-    
+        number = -number  # Prendre la valeur absolue pour gérer les négatifs
+    last_digit = number % 10  # Extraire le dernier chiffre
+    print(last_digit, end="")  # Affiche le dernier chiffre sans nouvelle ligne
+    return last_digit  # Retourne la valeur du dernier chiffre
