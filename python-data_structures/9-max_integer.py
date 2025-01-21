abd33ec32a4d1c4913f69a4d8d36b271189fe506
kanga-prog <kangabricekouakou@gmail.com>
2025-01-21 22:41:24 +0000

    9-max_integer.py

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,8 +3,8 @@
 def max_integer(my_list=[]):
     if not my_list:  # Check if the list is empty
         return None
-    max_val = my_list[0] 
+    max_val = my_list[0]
     for num in my_list:  # Iterate through the list
         if num > max_val:
-            max_val = num    
+            max_val = num
     return max_val  # Return the largest value found
