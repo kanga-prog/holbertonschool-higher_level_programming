ebd0dd1dc20ec756a5ca66d53e14c03eb82de3c3
kanga-prog <kangabricekouakou@gmail.com>
2025-01-21 22:58:36 +0000

    11-delete_at.py

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >= len(my_list):
-        return my_list  # Return the list unchanged    
-    # Use slicing to delete the item at idx and return the modified list
-    return my_list[:idx] + my_list[idx+1:]
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    del (my_list[idx])
+    return my_list
