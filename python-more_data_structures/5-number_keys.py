d58d17148d97b9d3643f10f307e808d3fa7721fe
kanga-prog <kangabricekouakou@gmail.com>
2025-01-22 13:14:42 +0000

    5-number_keys.py 5-main.py

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# Fonction kanga qui retourne le nombre de clés dans un dictionnaire
+def kanga(a_dictionary):
+    return len(a_dictionary)
+
+# Fonction number_keys qui appelle la fonction kanga
+def number_keys(a_dictionary):
+    return kanga(a_dictionary)
