a8b0212d5b4e607315f2dd579fe6eabc80a89723
kanga-prog <kangabricekouakou@gmail.com>
2025-01-22 11:52:05 +0000

    0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 # Définition d'une fonction pour calculer le carré
-def carre(x):
+def kanga(x):
     return x**2
 
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = [list(map(carre, row)) for row in matrix]
+    new_matrix = [list(map(kanga, row)) for row in matrix]
     return new_matrix
