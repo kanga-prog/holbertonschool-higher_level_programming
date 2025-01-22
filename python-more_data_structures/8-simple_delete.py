9ac9cf88fbf9a102745f1206c61870f2a0134d44
kanga-prog <kangabricekouakou@gmail.com>
2025-01-22 14:56:09 +0000

    8-simple_delete.py 8-main.py

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-    # Vérifie si la clé existe dans le dictionnaire
-    if key in a_dictionary:
-        # Si elle existe, la supprimer
-        del a_dictionary[key]
-    # Retourne le dictionnaire (qu'il ait été modifié ou non)
+
+    a_dictionary.pop(key, None)
     return a_dictionary
