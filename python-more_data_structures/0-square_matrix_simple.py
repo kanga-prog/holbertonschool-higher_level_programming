9b27fd8867eff4824f656d721c0ef2976913cbcf
kanga-prog <kangabricekouakou@gmail.com>
2025-01-22 00:32:25 +0000

    0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
+
+def square(x):
+    return x ** 2
 def square_matrix_simple(matrix=[]):
-    return [list(map(lambda x: x ** 2, row)) for row in matrix]
+    return [list(map(square, row)) for row in matrix]
