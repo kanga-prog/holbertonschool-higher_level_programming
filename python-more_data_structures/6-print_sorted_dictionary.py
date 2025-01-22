4f2d9d96530dd018c4f5a7a17e34f62c3a39c644
kanga-prog <kangabricekouakou@gmail.com>
2025-01-22 13:41:53 +0000

    6-print_sorted_dictionary.py  6-main.py

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def kanga(a_dictionary):
+    # Retourne les clés triées du dictionnaire
+    return sorted(a_dictionary.keys())
+
+
+def print_sorted_dictionary(a_dictionary):
+    # Obtenez les clés triées via la fonction kanga
+    sorted_keys = kanga(a_dictionary)
+    
+    # Afficher chaque clé et sa valeur
+    for key in sorted_keys:
+        print(f"{key}: {a_dictionary[key]}")
