b48e1269b0f037d9437bef3bc6c0abfc25560fb3
kanga-prog <kangabricekouakou@gmail.com>
2025-01-22 11:49:28 +0000

    0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,7 +4,7 @@
 def carre(x):
     return x**2
 
+
 def square_matrix_simple(matrix=[]):
-    # Utilisation de la fonction 'carre' à la place de 'lambda'
     new_matrix = [list(map(carre, row)) for row in matrix]
     return new_matrix
