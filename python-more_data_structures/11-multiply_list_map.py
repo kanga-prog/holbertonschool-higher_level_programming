f84a0ac5816e0dd262f2cafdc289cd7c22482742
kanga-prog <kangabricekouakou@gmail.com>
2025-01-22 15:34:26 +0000

    11-multiply_list_map.py

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
+def kanga(num):
+    return num * 4
+
 def multiply_list_map(my_list=[], number=0):
-    return list(map(lambda x: x * number, my_list))
+    return list(map(kanga, my_list))
