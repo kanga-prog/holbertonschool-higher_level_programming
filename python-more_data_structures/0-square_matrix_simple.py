342cea15a3454eae679667244347735382062f1e
kanga-prog <kangabricekouakou@gmail.com>
2025-01-22 10:56:01 +0000

    0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[x**2 for x in row] for row in matrix]
+    # Créer une copie de la matrice pour éviter de modifier l'originale
+    new_matrix = [list(map(lambda x: x**2, row)) for row in matrix]
     return new_matrix
