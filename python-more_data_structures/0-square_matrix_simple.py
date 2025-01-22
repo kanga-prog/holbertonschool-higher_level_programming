970c8886f7913043c87935588611f5ee5dd08378
kanga-prog <kangabricekouakou@gmail.com>
2025-01-22 00:27:41 +0000

    0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    # Crée une nouvelle matrice avec les carrés des éléments
-    new_matrix = []
-    for row in matrix:
-        new_matrix.append([element ** 2 for element in row])
-    return new_matrix
+    return [list(map(lambda x: x ** 2, row)) for row in matrix]
