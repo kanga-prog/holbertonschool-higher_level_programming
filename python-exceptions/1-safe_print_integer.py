880e85fc277248dcdbd29f2fb9c2b80837578bde
kanga-prog <kangabricekouakou@gmail.com>
2025-01-23 12:02:07 +0000

    1-safe_print_integer.py

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,13 +1,5 @@
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
-        return True  # Return True if printing is successful
-    except (ValueError, TypeError):
-        return False  # Return False if an error occurs
-
-
-def safe_print_integer(value):
-    try:
         # Essayer de convertir la valeur en entier
         print("{:d}".format(int(value)))
         return True  # Si la conversion et l'impression réussissent, retourner True
