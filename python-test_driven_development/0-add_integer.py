f7f96e187eae0d9fc047707d89e7bc7f367b9813
kanga-prog <kangabricekouakou@gmail.com>
2025-01-24 22:06:35 +0000

    0-add_integer.py 0-main.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 
 def add_integer(a, b=98):
+    """Returns the sum of two integers, a and b. If a or b are floats, they will be cast to integers.
+    
+    Raises:
+        TypeError: If either a or b is not an integer or float.
+    """
+    
+    # Ensure a and b are either integers or floats
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
     
+    # Cast a and b to integers if they are floats
     a = int(a)
     b = int(b)
     
