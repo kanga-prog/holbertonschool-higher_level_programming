ae15347772757ec3bcafae2151b62770ed66d461
kanga-prog <kangabricekouakou@gmail.com>
2025-01-24 13:46:50 +0000

    4-main.py 4-list_division.py

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,23 @@
+#!/usr/bin/pyhon3
+
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            if i >= len(my_list_1) or i >= len(my_list_2):
+                print("out of range")
+                result.append(0)
+            else:
+                if isinstance(my_list_1[i], (int, float)) and isinstance(my_list_2[i], (int, float)):
+                    try:
+                        result.append(my_list_1[i] / my_list_2[i])
+                    except ZeroDivisionError:
+                        print("division by 0")
+                        result.append(0)
+                else:
+                    print("wrong type")
+                    result.append(0)
+        except IndexError:
+            print("out of range")
+            result.append(0)
+    return result
