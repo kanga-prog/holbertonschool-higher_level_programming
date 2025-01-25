2b0d0b22c51bb7c4148a64e0e202508579471331
kanga-prog <kangabricekouakou@gmail.com>
2025-01-25 16:18:25 +0000

    3-say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,14 +3,12 @@
 def say_my_name(first_name, last_name=""):
     """
     Prints "My name is <first_name> <last_name>".
-    
     Parameters:
     first_name (str): The first name of the person.
-    last_name (str, optional): The last name of the person. Defaults to an empty string.
-    
+    last_name (str, optional): The last name of the person.\
+          Defaults to an empty string.
     Raises:
     TypeError: If either `first_name` or `last_name` is not a string.
-    
     Example:
     >>> say_my_name("John", "Smith")
     My name is John Smith
