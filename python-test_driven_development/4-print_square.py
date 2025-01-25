045491701e98e47db1c8683a3852d1e27ff1f1c9
kanga-prog <kangabricekouakou@gmail.com>
2025-01-25 16:43:48 +0000

    exo4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 def print_square(size):
     """
     Prints a square with the character #.
@@ -15,7 +17,6 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    
     if size == 0:
         return  # No output for size 0
 
