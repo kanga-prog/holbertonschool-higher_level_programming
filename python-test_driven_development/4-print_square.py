898c9760552ecbe3190e3782802c865f1d0c2dfa
kanga-prog <kangabricekouakou@gmail.com>
2025-01-25 16:36:36 +0000

    exo 4

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def print_square(size):
+    """
+    Prints a square with the character #.
+
+    Parameters:
+    size (int): The size of the square.
+
+    Raises:
+    TypeError: If size is not an integer or is a negative float.
+    ValueError: If size is less than 0.
+    """
+    if not isinstance(size, int):  # Check if size is an integer
+        if isinstance(size, float) and size < 0:
+            raise TypeError("size must be an integer")
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
