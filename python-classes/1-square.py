0e20d7fb09f16d21f5a5d1abf9323fed91041a46
kanga-prog <9952@holbertonstudents.com>
2025-01-29 01:48:24 +0100

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -9,5 +9,18 @@ and ensures the value is a non-negative integer.
 
 
 class Square:
+    """
+    Defines a square by its size.
+
+    Attributes:
+        __size (int): The size of the side of the square.
+    """
+
     def __init__(self, size):
+        """
+        Initializes an instance of the Square class.
+
+        Args:
+            size (int): The size of the side of the square.
+        """
         self.__size = size
