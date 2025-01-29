d9974ff1d2094cc67682ac81a99fc227b74472a7
kanga-prog <9952@holbertonstudents.com>
2025-01-29 02:04:00 +0100

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 """
-This module defines a Square class that represents a square.
-
+This module defines an empty Square class.
 """
 
 
 class Square:
+    """
+    An empty class that defines a square.
+    """
     pass
