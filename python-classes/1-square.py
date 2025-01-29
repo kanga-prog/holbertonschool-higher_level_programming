d21e7ed19b3f205e101081d722c709227517e0a2
kanga-prog <9952@holbertonstudents.com>
2025-01-29 01:42:04 +0100

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 
+"""
+This module defines a Square class that represents a square.
+
+It includes an initialization method that accepts a side length
+and ensures the value is a non-negative integer.
+"""
+
+
 class Square:
     def __init__(self, size):
         self.__size = size
