1ed8c473eb1a207730b9ba12fed646b9e0d64483
kanga-prog <9952@holbertonstudents.com>
2025-01-31 19:13:53 +0100

    essaie

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,118 +1,69 @@
 #!/usr/bin/python3
 
-""" ce module traite d'un rectangnle """
-
-
 class Rectangle:
-    """
-    A class to represent a rectangle.
-
-    Attributes:
-        width (int): The width of the rectangle.
-        height (int): The height of the rectangle.
-
-    Methods:
-        area(): Returns the area of the rectangle.
-        perimeter(): Returns the perimeter of the rectangle.
-        __str__(): Returns a string representation of the rectangle.
-        __repr__(): Returns a string with a representation of the object.
-    """
-
     def __init__(self, width=0, height=0):
-        """
-        Initializes the rectangle with optional width and height.
-
-        Args:
-            width (int): The width of the rectangle (default is 0).
-            height (int): The height of the rectangle (default is 0).
-        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Getter for width attribute."""
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
-        """
-        Setter for width attribute.
-
-        Args:
-            value (int): The value to set the width.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than 0.
-        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self._width = value
+        self.__width = value
 
     @property
     def height(self):
-        """Getter for height attribute."""
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
-        """
-        Setter for height attribute.
-
-        Args:
-            value (int): The value to set the height.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than 0.
-        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self._height = value
+        self.__height = value
 
     def area(self):
-        """
-        Calculates and returns the area of the rectangle.
-
-        Returns:
-            int: The area of the rectangle.
-        """
-        return self._width * self._height
+        return self.width * self.height
 
     def perimeter(self):
-        """
-        Calculates and returns the perimeter of the rectangle.
-
-        Returns:
-            int: The perimeter of the rectangle, or 0 if either
-                 width or height is 0.
-        """
-        if self._width == 0 or self._height == 0:
+        if self.width == 0 or self.height == 0:
             return 0
-        return 2 * (self._width + self._height)
+        return 2 * (self.width + self.height)
 
     def __str__(self):
-        """
-        Returns a string representation of the rectangle using '#'.
-
-        Returns:
-            str: A string representing the rectangle, or an empty
-                 string if either width or height is 0.
-        """
-        if self._width == 0 or self._height == 0:
+        """ Retourne une chaîne représentant le rectangle avec # """
+        if self.width == 0 or self.height == 0:
             return ""
-        return "\n".join(["#" * self._width] * self._height)
+        rectangle_str = "\n".join([str(self.print_symbol) * self.width for _ in range(self.height)])
+        return rectangle_str + f"\n<{self.__class__.__name__} object at {hex(id(self))}>"
 
-    def __repr__(self):
-        """
-        Returns a string with a representation of the object.
+    # Attribut de classe
+    print_symbol = "#"
 
-        Returns:
-            str: A string representing the object with its memory address.
-        """
-        return ("<3-Rectangle.{} object at {:#x}>".format(self.__class__.__name__, id(self)))
+    def __del__(self):
+        """ Affiche un message lors de la suppression d'une instance """
+        print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """ Retourne le plus grand rectangle en fonction de l'aire """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        """ Retourne un carré avec un côté de taille size """
+        return cls(size, size)
