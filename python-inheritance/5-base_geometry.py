7e9496c87c2be53abb96075dcb3bbe4eafdaa606
kanga-prog <9952@holbertonstudents.com>
2025-02-05 16:11:01 +0100

    exo 5

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 """
-Module concernant
+Module est vide 
 """
+
+
 class BaseGeometry:
+    """
+    A base class for geometry objects.
+
+    This class serves as a base for future geometry-related classes. 
+    It does not currently implement any methods or attributes, but it can
+    be extended to include geometric operations and properties.
+    """
     pass
