a3af9f959fc916e21f9e9f3531f6752862485b50
kanga-prog <9952@holbertonstudents.com>
2025-02-05 12:20:13 +0100

    exo1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,22 +1,35 @@
 #!/usr/bin/python3
 
 """
-ce module contient une class heritage d'une autre class nomé list.
+Ce module contient une classe `MyList` qui hérite de la classe intégrée `list`.
 
+La classe `MyList` ajoute une méthode permettant d'afficher la liste triée
+sans modifier l'ordre des éléments dans la liste d'origine.
+
+Cette fonctionnalité est utile lorsqu'on veut afficher une version triée de la
+liste sans affecter la liste originale.
 """
+
+
 class MyList(list):
     """
-    A class that inherits from the built-in list class.
-    
-    This class provides a method to print the list in sorted order without
-    modifying the original list.
+    Une classe qui hérite de la classe intégrée `list`.
+    Cette classe fournit une méthode `print_sorted` qui permet d'afficher la
+    liste triée sans modifier la liste d'origine. La méthode utilise la
+    fonction `sorted()` pour obtenir une version triée de la liste.
     """
 
     def print_sorted(self):
         """
-        Prints the list in ascending order without modifying the original list.
-        
-        The list is sorted using the built-in sorted() function, which returns
-        a new list, leaving the original list unchanged.
+        Affiche la liste dans un ordre croissant sans modifier\
+              la liste d'origine.
+        La méthode utilise la fonction `sorted()` pour retourner une nouvelle
+        liste triée. L'ordre de la liste originale reste inchangé.
+        Exemple:
+            >>> my_list = MyList([3, 1, 4, 5, 2])
+            >>> my_list.print_sorted()
+            [1, 2, 3, 4, 5]
+            >>> print(my_list)
+            [3, 1, 4, 5, 2]
         """
         print(sorted(self))
