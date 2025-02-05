2b2ce2cf8952cac44570fa5d20d3fabbfb66375c
kanga-prog <9952@holbertonstudents.com>
2025-02-05 13:51:56 +0100

    exo2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 
 """
-Ce module contient une fonction `is_same_class` qui permet de vérifier si un objet
+Ce module contient une fonction `is_same_class`\
+      qui permet de vérifier si un objet
 est exactement une instance d'une classe donnée.
 
-La fonction compare l'objet avec la classe spécifiée et retourne `True` si l'objet
+La fonction compare l'objet avec la classe spécifiée\
+      et retourne `True` si l'objet
 est une instance directe de cette classe, sans tenir compte des sous-classes.
 
 Cette fonctionnalité est utile lorsque vous avez besoin de savoir si un objet
 appartient à une classe particulière et non à une sous-classe.
 
-La fonction ne prend pas en compte l'héritage, elle vérifie uniquement l'appartenance
+La fonction ne prend pas en compte l'héritage,\
+      elle vérifie uniquement l'appartenance
 directe à la classe.
 
 Exemple d'utilisation :
@@ -24,6 +27,8 @@ Exemple d'utilisation :
 """
 
 def is_same_class(obj, a_class):
+
+
     """
     Vérifie si un objet est exactement une instance d'une classe spécifiée.
 
