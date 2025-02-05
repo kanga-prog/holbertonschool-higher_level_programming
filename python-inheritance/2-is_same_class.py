70f7b98b57866cb4ffd428701a92c1b6936ac37a
kanga-prog <9952@holbertonstudents.com>
2025-02-05 13:49:14 +0100

    exo 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,22 +1,41 @@
-"""
-Ce module traite d'une fonction qui verifie si un objet appartient\
-      a une class et renvoie true si c'est vrais et faux si c'est faux
-"""
+#!/usr/bin/python3
 
+"""
+Ce module contient une fonction `is_same_class` qui permet de vérifier si un objet
+est exactement une instance d'une classe donnée.
+
+La fonction compare l'objet avec la classe spécifiée et retourne `True` si l'objet
+est une instance directe de cette classe, sans tenir compte des sous-classes.
+
+Cette fonctionnalité est utile lorsque vous avez besoin de savoir si un objet
+appartient à une classe particulière et non à une sous-classe.
+
+La fonction ne prend pas en compte l'héritage, elle vérifie uniquement l'appartenance
+directe à la classe.
+
+Exemple d'utilisation :
+
+    >>> is_same_class(1, int)
+    True
+    >>> is_same_class(1, float)
+    False
+    >>> is_same_class("hello", str)
+    True
+"""
 
 def is_same_class(obj, a_class):
     """
-    Checks if an object is exactly an instance of a specified class.
+    Vérifie si un objet est exactement une instance d'une classe spécifiée.
 
-    Args:
-        obj: The object to check.
-        a_class: The class to compare the object against.
+    Arguments:
+        obj: L'objet à vérifier.
+        a_class: La classe à comparer avec l'objet.
 
-    Returns:
-        True if the object is an instance of the specified class, 
-        False otherwise.
+    Retourne:
+        True si l'objet est une instance de la classe spécifiée,
+        False sinon.
 
-    Example:
+    Exemple:
         >>> is_same_class(1, int)
         True
         >>> is_same_class(1, float)
@@ -24,7 +43,7 @@ def is_same_class(obj, a_class):
         >>> is_same_class("hello", str)
         True
     """
-    if type(obj) == a_class:
+    if obj.__class__ is a_class:
         return True
     else:
         return False
