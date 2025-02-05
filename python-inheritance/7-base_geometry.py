c3bac8585c3e0a39c088bd438457a18c97a2bec8
kanga-prog <9952@holbertonstudents.com>
2025-02-05 16:54:39 +0100

    7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -9,19 +9,19 @@ class BaseGeometry:
     """
     A class representing basic geometry.
 
-    This class provides methods for validating integers and raising an exception
+    This class provides methods for validating integers\
+          and raising an exception
     for the `area()` method, which is not implemented in the base class.
     """
-    
     def area(self):
         """
         Raises an exception indicating that the area method is not implemented.
 
         This method is meant to be overridden in subclasses, as the calculation
         of the area depends on the specific geometry.
-        
         Raises:
-            Exception: Always raises an exception stating that the method is not implemented.
+            Exception: Always raises an exception stating\
+                  that the method is not implemented.
         """
         raise Exception("area() is not implemented")
 
@@ -32,12 +32,11 @@ class BaseGeometry:
         Args:
             name (str): The name of the variable being validated.
             value (int): The value to be validated.
-        
         Raises:
             TypeError: If `value` is not an integer.
             ValueError: If `value` is less than or equal to 0.
-        
-        The method checks if the value is an integer and if it is greater than 0.
+        The method checks if the value is an integer and \
+            if it is greater than 0.
         """
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
