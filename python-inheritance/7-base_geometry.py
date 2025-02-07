94e8a318e84b6fbed57f46bf088eb931092fc7cd
kanga-prog <9952@holbertonstudents.com>
2025-02-07 14:22:42 +0100

    7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -39,7 +39,7 @@ class BaseGeometry:
         The method checks if the value is an integer and \
             if it is greater than 0.
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
