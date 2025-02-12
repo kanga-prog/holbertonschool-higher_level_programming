688317e3fd100db80b8cf177fa0ab9023ba93849
kanga-prog <9952@holbertonstudents.com>
2025-02-12 15:25:17 +0100

    exo 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,9 +1,10 @@
-#/usr/bin/python
+#!/usr/bin/python3
+"""Module containing append_file function"""
+
 
 def append_write(filename="", text=""):
-    # Open the file in append mode ('a') with UTF-8 encoding, create it if it doesn't exist
-    with open(filename, 'a', encoding='utf-8') as file:
-        # Write the given text to the file
-        file.write(text)
-        # Return the number of characters added
-        return len(text)
+    """The append_file function"""
+
+    # Opening and appending to the file
+    with open(filename, 'a', encoding='UTF-8') as f:
+        return f.write(text)
