2edcaff1fb0b8f4d440c5178dcd17736f89a46df
kanga-prog <9952@holbertonstudents.com>
2025-02-12 15:14:51 +0100

    exo4

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,20 +1,8 @@
 #!/usr/bin/python3
-
+"""Module containing from_json_string function"""
 import json
 
+
 def from_json_string(my_str):
-    """
-    Converts a JSON string to a Python object.
-
-    Args:
-        my_str (str): A string representing a JSON object.
-
-    Returns:
-        object: The Python object represented by the JSON string.
-    
-    Example:
-        s = '{"name": "John", "age": 30}'
-        obj = from_json_string(s)
-        print(obj)  # Output: {'name': 'John', 'age': 30}
-    """
+    """Function to return obj representation of JSON"""
     return json.loads(my_str)
