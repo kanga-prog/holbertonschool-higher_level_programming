702e473a95052c35a648f6eaccb7451ef7ff50ab
kanga-prog <9952@holbertonstudents.com>
2025-02-12 15:34:30 +0100

    exo 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
+"""Module containing read_file function"""
+
 
 def read_file(filename=""):
-    """
-    Reads a text file (UTF-8) and prints its contents to stdout.
+    """The read_file function"""
 
-    Args:
-        filename (str): The name of the file to read. Defaults to 'tests/my_file_0.txt'.
-                         If no filename is provided, it will read 'tests/my_file_0.txt'.
-
-    Returns:
-        None: This function does not return any value; it only prints the contents of the file.
-
-    Usage:
-        read_file()  # Reads 'tests/my_file_0.txt' and prints its contents to stdout.
-        read_file("other_file.txt")  # Reads 'other_file.txt' and prints its contents to stdout.
-    """
-    with open('tests/my_file_0.txt', 'r',encoding='utf-8') as file:
-        print(file.read(), end="")
+    # Openning and reading file
+    with open(filename, 'r', encoding='UTF-8') as f:
+        read_data = f.read()
+        print(read_data, end="")
