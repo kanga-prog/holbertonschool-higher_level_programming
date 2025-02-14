fcf67c0bab614f42d71505c6ee7da4a777417b95
kanga-prog <9952@holbertonstudents.com>
2025-02-14 19:54:38 +0100

    a class Student that defines a student by: (based on 10-student.py)

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,36 +1,30 @@
 #!/usr/bin/python3
+"""Module containing Student class"""
+
 
 class Student:
-    def __init__(self, first_name, last_name, age):
-        """
-        Initializes a new student instance.
+    """The Student class"""
 
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
+    def __init__(self, first_name, last_name, age):
+        """Initialization of the class instance"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-        Retrieves a dictionary representation of a Student instance.
-
-        Args:    attrs (list): A list of attributes to retrieve.
-        Returns:
-            dict: A dictionary with the student attributes.
-        """
-        if attrs is None:
-            return self.__dict__
-        else:
-            return {key: getattr(self, key) for key in attrs
-                    if hasattr(self, key)}
+        """Method to retrieve dict elements of the class"""
+        if isinstance(attrs, list) and \
+                all(isinstance(element, str) for element in attrs):
+            new_dict = dict()
+            for i in attrs:
+                if i in self.__dict__:
+                    new_dict[i] = self.__dict__[i]
+            return new_dict
+        return self.__dict__
 
     def reload_from_json(self, json):
-        """
-        Replaces all attributes of the Student instance using a dictionary.
-        """
-        for key, value in json.items():
-            setattr(self, key, value)
+        """Method to change JSON"""
+        for key in self.__dict__.keys():
+            for key2 in json.keys():
+                if key == key2:
+                    self.__dict__[key] = json[key2]
