2b3b79d9c573c3b19ae7bd40704079dfaf9396cc
kanga-prog <9952@holbertonstudents.com>
2025-03-06 22:12:30 +0100

    Create the user user_0d_1 and Grant all privileges

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,9 @@
+-- Create the user 'user_0d_1' if it does not already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to 'user_0d_1' on all databases and tables
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Apply the changes
+FLUSH PRIVILEGES;
+
