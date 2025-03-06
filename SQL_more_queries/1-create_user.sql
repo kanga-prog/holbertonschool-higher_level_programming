b901a43b9d9b65cb2d05f977cf7f72eec2c7faa4
kanga-prog <9952@holbertonstudents.com>
2025-03-06 22:41:52 +0100

    test

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
+-- task : a script that creates the MySQL server user user_0d_1
+
 -- Create the user 'user_0d_1' if it does not already exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
