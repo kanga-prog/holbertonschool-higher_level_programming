70c7133575f79c201b8b5db04c6ba91ba3769bb6
kanga-prog <9952@holbertonstudents.com>
2025-03-07 14:25:11 +0100

    Script to list all TV shows that have no genre linked in the database hbtn_0d_tvshows

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,10 +1,7 @@
--- Script to list all TV shows that have no genre linked in the database hbtn_0d_tvshows
--- Each record will display tv_shows.title and tv_show_genres.genre_id as NULL for shows without a genre
--- The results will be sorted by tv_shows.title in ascending order
+-- Lists all shows contained in `hbtn_0d_tvshows`
+-- without a genre linked.
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title ASC;
-
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
