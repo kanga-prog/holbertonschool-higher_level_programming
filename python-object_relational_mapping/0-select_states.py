bcf8d359f3f47b2b8eca2fa60583563e4ad65a18
kanga-prog <9952@holbertonstudents.com>
2025-03-07 23:17:56 +0100

    a script that lists all states from the database

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-
+'''
+This module  lists all states from the database hbtn_0e_0_usa
+'''
 import MySQLdb
 import sys
-
-# Module to list all states from the database hbtn_0e_0_usa
 
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    db_name = sys.argv[3]
+    database = sys.argv[3]
 
     db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=db_name)
-
+                         user=username, passwd=password, db=database)
     cursor = db.cursor()
-    cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
     states = cursor.fetchall()
 
     for state in states:
