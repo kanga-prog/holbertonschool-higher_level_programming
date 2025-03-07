46a1eb674ca60003e19693bb18b938f27b416f39
kanga-prog <9952@holbertonstudents.com>
2025-03-07 14:12:14 +0100

    Import the database dump of hbtn_0d_tvshows

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,9 @@
+-- Script to list all TV shows and their associated genre IDs from the database hbtn_0d_tvshows
+-- The result includes shows without genres, displaying NULL for the genre_id if no genre is linked
+-- The results are sorted by TV show title (ascending) and genre_id (ascending)
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+
