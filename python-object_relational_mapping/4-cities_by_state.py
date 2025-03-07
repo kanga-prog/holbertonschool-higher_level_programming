ed36e7b1c49f119ab43eaca0d643c370a6ffa683
kanga-prog <9952@holbertonstudents.com>
2025-03-07 23:51:53 +0100

    lists all cities from the database hbtn_0e_4_usa

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
 '''
-This module takes in arguments and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument.
-But this time, write one that is safe from MySQL injections!
+ lists all cities from the database hbtn_0e_4_usa
 '''
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
 
-    if len(sys.argv) != 5:
+    if len(sys.argv) != 4:
         exit(1)
 
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-    state_name = sys.argv[4]
 
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=username, passwd=password, db=db_name)
     cursor = db.cursor()
 
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    query = """
+    SELECT cities.id, cities.name, states.name
+    FROM cities
+    JOIN states ON cities.state_id = states.id
+    ORDER BY cities.id ASC
+    """
 
-    cursor.execute(query, (state_name,))
+    cursor.execute(query)
 
     states = cursor.fetchall()
 
