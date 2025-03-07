cc965d67b8225d69ba8d1b59316d389313e8fc29
kanga-prog <9952@holbertonstudents.com>
2025-03-07 23:30:47 +0100

    lists all states starting with N from the database

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,34 +1,13 @@
 #!/usr/bin/python3
-
-"""
-Script that connects to a MySQL database and displays all reports
-whose name starts with 'N', sorted by ID.
-"""
+'''
+This module  lists all states starting with N from the database hbtn_0e_0_usa
+'''
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Récupérer les arguments de la ligne de commande
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
 
-    # Se connecter au serveur MySQL en localhost au port 3306
-    db = MySQLdb.connect(
-        host="localhost", port=3306, user=username, passwd=password, db=db_name
-    )
-
-    cursor = db.cursor()
-
-    # Requête pour récupérer les états dont le nom commence par 'N'
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-    cursor.execute(query)
-
-    # Récupérer et afficher les résultats
-    results = cursor.fetchall()
-    for row in results:
-        print(row)
-
-    # Fermer le curseur et la connexion à la base de données
-    cursor.close()
-    db.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
