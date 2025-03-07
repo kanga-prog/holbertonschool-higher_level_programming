71054af4ce8e3054432c14cc2dcba68f983736d7
kanga-prog <9952@holbertonstudents.com>
2025-03-07 13:06:43 +0100

    exo2

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,11 +1,6 @@
--- 1-create_and_grant_user.sql
-
--- Créer l'utilisateur 'user_0d_1' s'il n'existe pas déjà
+-- Creates a MySQL Server user `user_0d_1`
+-- and sets a temporary password `user_0d_1_pwd`.
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Accorder tous les privilèges à 'user_0d_1' sur toutes les bases de données
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Appliquer les modifications en rechargeant les privilèges
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
-
