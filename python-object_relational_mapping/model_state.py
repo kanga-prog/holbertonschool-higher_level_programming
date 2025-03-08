3bc40a2ba85276d59cfbc19595bd15a9cfe60b89
kanga-prog <9952@holbertonstudents.com>
2025-03-08 23:24:59 +0100

    Script that defines the State class and links it to the  table in a MySQL database

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script that defines the State class and links it to the `states` table in a MySQL database.
+Script that defines the State class and links \
+    it to the `states` table in a MySQL database.
 """
 import sys
 from sqlalchemy import Column, Integer, String
@@ -9,6 +10,7 @@ from sqlalchemy import create_engine
 
 # Declare the Base class for all models
 Base = declarative_base()
+
 
 class State(Base):
     """
@@ -20,13 +22,15 @@ class State(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
+
 if __name__ == "__main__":
+
     # Establish connection to MySQL using user credentials and database name
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
         pool_pre_ping=True
     )
-    
     # Create the states table if it doesn't exist
     Base.metadata.create_all(engine)
-
