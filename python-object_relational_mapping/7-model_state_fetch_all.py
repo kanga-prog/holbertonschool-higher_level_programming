2709f7f2614a9b723e07366727b4f5f2d85ac722
kanga-prog <9952@holbertonstudents.com>
2025-03-08 23:36:17 +0100

    Script that lists all State objects from the database hbtn_0e_6_usa.

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,9 +10,9 @@ from model_state import Base, State
 if __name__ == "__main__":
     # Establish connection to MySQL
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
-                                                   sys.argv[2],
-                                                   sys.argv[3]),
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
         pool_pre_ping=True
     )
 
