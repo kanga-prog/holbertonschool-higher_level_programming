21f5856e6c772de0ffb7f4475f2b6f348c0ff6b0
kanga-prog <9952@holbertonstudents.com>
2025-03-08 20:16:12 +0100

    Script that lists all states starting by one letter

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Script that lists all states with a name starting\
-      with 'N' from a MySQL database.
+Script that lists all states with a name starting
+with 'N' from a MySQL database.
 It takes three arguments: mysql username, mysql password, and database name.
 The results are sorted by states.id in ascending order.
 """
@@ -10,25 +10,38 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    """
+    Main function to connect to MySQL, execute the query and list states
+    starting with 'N' ordered by their id.
+    """
+    
     # Get the arguments from the command line
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
+    # Connect to the MySQL server
     db = MySQLdb.connect(
-                     host="localhost",
-                     port=3306, user=username,
-                     passwd='KKb', db=database
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
     )
 
+    # Create a cursor to execute queries
     cursor = db.cursor()
 
+    # Execute the query to get states starting with 'N'
     cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
+    # Fetch all rows from the executed query
     states = cursor.fetchall()
 
+    # Print each state
     for state in states:
         print(state)
 
+    # Close the cursor and the database connection
     cursor.close()
     db.close()
