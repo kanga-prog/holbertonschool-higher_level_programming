3daddcf4bfd01743f7d19ed9dcf648ad32427516
kanga-prog <9952@holbertonstudents.com>
2025-03-08 20:51:26 +0100

    Script that lists all states starting by one letter

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,75 +1,13 @@
 #!/usr/bin/python3
-"""
-This script lists all states with a name starting with 'N' from a MySQL database.
-It takes three arguments: mysql username, mysql password, and database name.
-The results are sorted by states.id in ascending order.
-"""
-
+'''
+This module  lists all states starting with N from the database hbtn_0e_0_usa
+'''
 import MySQLdb
 import sys
 
-
-def list_states_starting_with_n(username, password, database):
-    """
-    Connects to the MySQL database, retrieves all states whose names start
-    with 'N', and returns them sorted by 'id' in ascending order.
-
-    Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        database (str): The name of the database.
-
-    Returns:
-        list: A list of tuples where each tuple contains the state id and name.
-    """
-    try:
-        # Establish connection to MySQL server
-        db = MySQLdb.connect(user=username, passwd=password, db=database)
-    except MySQLdb.MySQLError as e:
-        print(f"Error: {e}")
-        return []
-
-    with db.cursor() as cursor:
-        # Execute the query to retrieve states starting with 'N'
-        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
-        # Fetch all rows and return the results
-        states = cursor.fetchall()
-
-    db.close()
-    return states
-
-
-def print_states(states):
-    """
-    Prints the states fetched from the database.
-
-    Args:
-        states (list): A list of tuples containing state id and name.
-    """
-    for state in states:
-        print(state)
-
-
 if __name__ == "__main__":
-    """
-    Main function to fetch and display states starting with 'N' from the database.
-    It expects three arguments: MySQL username, password, and database name.
-    """
-    if len(sys.argv) != 4:
-        print("Usage: ./1-filter_states.py <mysql_username> <mysql_password> <database_name>")
-        sys.exit(1)
 
-    # Get the arguments from the command line
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Fetch the states starting with 'N'
-    states = list_states_starting_with_n(username, password, database)
-
-    # If we have states, print them
-    if states:
-        print_states(states)
-    else:
-        print("No states found.")
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
