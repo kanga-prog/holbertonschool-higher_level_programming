08df29bf99719d77861368ec1fbc00c0d75f3e7a
kanga-prog <9952@holbertonstudents.com>
2025-03-09 11:28:00 +0100

    Displays all cities in the database with their state

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -10,16 +10,23 @@ from model_state import Base
 
 if __name__ == "__main__":
     # Connexion à la base de données MySQL
-    engine = create_engine(f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}")
+    engine = create_engine(
+        f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+    )
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
     # Requête pour récupérer les villes avec leur état correspondant
-    results = session.query(State.name, City.id, City.name).join(City).order_by(City.id).all()
+    results = (
+        session.query(State.name, City.id, City.name)
+        .join(City)
+        .order_by(City.id)
+        .all()
+    )
 
     # Affichage des résultats
     for state_name, city_id, city_name in results:
         print(f"{state_name}: ({city_id}) {city_name}")
 
     session.close()
-
