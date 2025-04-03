4318d996d4c4a6b707ba6e9d427688698cf13432
kanga-prog <9952@holbertonstudents.com>
2025-04-03 16:52:43 +0200

    Click and turn red

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,5 +1,5 @@
 const redHeader = document.querySelector('#red_header');
 const header = document.querySelector('header');
-redHeader.addEventListener('click', function() {
+redHeader.addEventListener('click', function () {
   header.style.color = '#FF0000';
 });
